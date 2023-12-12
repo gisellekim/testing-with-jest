a6f86ad9fa2e51e93a53cb31589fae61cff4ca91
giselle <giselle.kim@eyos.one>
2023-12-12 23:57:37 +0000

    test: add a test for app component with history

---
+++ src/App.spec.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react"
+import { createMemoryHistory } from "history"
+import { Router } from "react-router-dom"
+import { App } from "./App"
+
+describe("App", () => {
+  it("renders successfuly", () => {
+    const history = createMemoryHistory()
+    const { container } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    )
+    expect(container.innerHTML).toMatch("Goblin Store")
+  })
+})
