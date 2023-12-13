bb6108235c38e9ef4aa729c7b2d2d44c0d0148b1
giselle <giselle.kim@eyos.one>
2023-12-13 00:24:36 +0000

    test: add mock Home component

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -3,14 +3,26 @@ import { createMemoryHistory } from "history"
 import { Router } from "react-router-dom"
 import { App } from "./App"
 
+jest.mock("./Home", () => ({ Home: () => <div>Home</div> }))
+
 describe("App", () => {
   it("renders successfuly", () => {
-    const history = createMemoryHistory()
+    const history = createMemoryHistory() // including URL, location
     const { container } = render(
       <Router history={history}>
         <App />
       </Router>
     )
     expect(container.innerHTML).toMatch("Goblin Store")
+  })
+  it("renders Home component on root route", () => {
+    const history = createMemoryHistory()
+    history.push("/") // push the root url to our history object before rendering the App component
+    const { container } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    )
+    expect(container.innerHTML).toMatch("Home")
   })
 })
