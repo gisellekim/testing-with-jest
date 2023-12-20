a4850995f4ea9bed5988c657e82d982f4d49aa2e
giselle <giselle.kim@eyos.one>
2023-12-20 15:52:14 +0000

    test: add a test for cart component

--- src/Cart/Cart.spec.tsx
+++ src/Cart/Cart.spec.tsx
@@ -1,6 +1,7 @@
-import { fireEvent } from "@testing-library/react"
+import { fireEvent, render } from "@testing-library/react"
 import { Cart } from "./Cart"
 import { useCartContext } from "../CartContext"
+import { CartItemProps } from "./CartItem"
 
 jest.mock("../CartContext", () => ({
   useCartContext: jest.fn(),
@@ -9,6 +10,17 @@ jest.mock("../CartContext", () => ({
 const useCartContextMock = useCartContext as unknown as jest.Mock<
   Partial<ReturnType<typeof useCartContext>>
 >
+
+jest.mock("./CartItem", () => ({
+  CartItem: ({ product }: CartItemProps) => {
+    const { name, price, image } = product
+    return (
+      <div>
+        {name} {price} {image}
+      </div>
+    )
+  },
+}))
 
 describe("Cart", () => {
   describe("Without products", () => {
@@ -40,14 +52,28 @@ describe("Cart", () => {
         },
         {
           name: "Product Giselle 2",
-          price: 100,
+          price: 200,
           image: "/image/product_2.png",
         },
       ]
-      useCartContextMock.mockReturnValue({ products, totalPrice: () => 55 })
+      useCartContextMock.mockReturnValue({ products, totalPrice: () => 500 })
+    })
+    it("renders cart products list with total price", () => {
+      const { container } = renderWithRouter(() => <Cart />)
+      expect(container.innerHTML).toMatch(
+        "Product Giselle 1 100 /image/product_1.png"
+      )
+      expect(container.innerHTML).toMatch(
+        "Product Giselle 2 200 /image/product_2.png"
+      )
+      expect(container.innerHTML).toMatch("Total: 500 Zm")
     })
     describe("on 'Go to checkout' click", () => {
-      it("redirects to 'checkout'", () => {})
+      it("redirects to 'checkout'", () => {
+        const { getByText, history } = renderWithRouter(() => <Cart />)
+        fireEvent.click(getByText("Go to checkout"))
+        expect(history.location.pathname).toBe("/checkout")
+      })
     })
   })
 })
